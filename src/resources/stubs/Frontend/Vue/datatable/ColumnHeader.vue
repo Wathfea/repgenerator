9f98431fba3696c88ab9654299d7f250868c4646
simontamas <simon.tamas@pentacom.hu>
2022-08-15 10:20:02 +0200

    Tables should now be overflow scroll x

--- src/resources/stubs/Frontend/Vue/datatable/ColumnHeader.vue
+++ src/resources/stubs/Frontend/Vue/datatable/ColumnHeader.vue
@@ -1,13 +1,12 @@
 <template>
   <div>
-    <SearchColumnPopup v-if="setSearchOpen" :data="data" :icon="icon" :set-search="search" :name="getName()" @close="onCloseSearch" @search="onSearch"/>
     <label for="email" class="block text-sm font-medium text-gray-700">{{ getName() }}</label>
     <div class="mt-1 rounded-md">
         <Button @click="onOpenSearch" class="-ml-px relative inline-flex items-center space-x-2 px-1 py-2 rounded-l-md border border-gray-300 text-sm font-medium text-gray-700 bg-gray-50 hover:bg-gray-100">
           <SearchCircleOutlineIcon v-if="!search" class="h-5 w-5 text-gray-400" aria-hidden="true"/>
           <SearchCircleIcon v-else class="h-5 w-5 text-vagheggi-800" aria-hidden="true"/>
         </Button>
-        <Button :disabled="!search && sortDirection === null" :no-opacity="true" :busy="data.isSearching"  @click="onSearchAndSortCleared" class="-ml-px relative inline-flex items-center space-x-2 px-1 py-2 border border-gray-300 text-sm font-medium text-gray-700 bg-gray-50 hover:bg-gray-100">
+        <Button :disabled="!search && sortDirection === null" :no-opacity="true" :busy="data.isSearching"  @click="onSearchAndSortCleared" class="-ml-px relative inline-flex items-center space-x-2 px-1 py-2 border border-gray-300  text-sm font-medium text-gray-700 bg-gray-50 hover:bg-gray-100">
           <XIcon :class="`h-5 w-5 text-${search !== '' || sortDirection !== null ? 'vagheggi-800' : 'gray-400'}`" aria-hidden="true"/>
         </Button>
         <Button @click="onSortChanged" type="button" class="-ml-px relative inline-flex items-center space-x-2 px-1 rounded-r-md py-2 border border-gray-300 text-sm font-medium text-gray-700 bg-gray-50 hover:bg-gray-100">
@@ -19,10 +18,9 @@
 </template>
 
 <script setup>
-import { SortAscendingIcon, SortDescendingIcon, XIcon, SearchIcon, SearchCircleIcon } from '@heroicons/vue/solid'
+import { SortAscendingIcon, SortDescendingIcon, XIcon, SearchCircleIcon } from '@heroicons/vue/solid'
 import { SearchCircleIcon as SearchCircleOutlineIcon }  from '@heroicons/vue/outline'
 import Button from "~/components/Button";
-import SearchColumnPopup from "~/components/DataTable/SearchColumnPopup";
 const emit = defineEmits(["changeSort", "changeSearch", "sortCleared", "closeSearch", "openSearch"]);
 let props = defineProps({
   data : {
