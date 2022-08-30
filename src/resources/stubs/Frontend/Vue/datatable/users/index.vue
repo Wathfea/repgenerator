3cfa46bdeb6ef4060dc4c95ead513c070f7efa55
simontamas <simon.tamas@pentacom.hu>
2022-08-30 16:00:40 +0200

    Add users pages

---
+++ src/resources/stubs/Frontend/Vue/datatable/users/index.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import CMSDashboard from "~/components/CMSDashboard.vue";
+import UserIndex from "~/components/User/UserIndex.vue";
+
+definePageMeta({ middleware: ["auth"] });
+</script>
+
+<template>
+  <NuxtLayout name="app-layout">
+    <Head>
+      <Title>Vagheggi CMS</Title>
+    </Head>
+    <CMSDashboard page="users" title="Felhasználók" :pages="[{href: '/users', name: 'Felhasználók', current: true, icon: 'UserIcon'}]">
+      <UserIndex/>
+    </CMSDashboard>
+  </NuxtLayout>
+</template>
